168a441844d2d2759f972bf5992ddb113c6689cd
reminderclock <reminderclock@naver.com>
2020-12-31 23:39:06 +0900

    Feat: [shape] 사다리꼴 넓이 구하기

--- shape/main.js
+++ shape/main.js
@@ -8,4 +8,9 @@ console.log(circle(2));
 function rectangle(width, height){
     return (width*height);
 }
-
+console.log(rectangle(3,4));
+// 사다리꼴의 넓이
+function trapezoid(upperSide, baseSide, height) {
+    return decideError((upperSide+baseSide)*height/2);
+}
+console.log(trapezoid(8, 12, 6));
