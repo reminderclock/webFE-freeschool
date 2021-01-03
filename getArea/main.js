47c92f154a07502764f33bb060fffed3519a214d
reminderclock <reminderclock@naver.com>
2021-01-04 00:44:34 +0900

    Fix: [getArea] 반지름 길이 변하지 않아도 실행

--- getArea/main.js
+++ getArea/main.js
@@ -1,7 +1,7 @@
 function getArea(shape, ...rest) {
         if(shape==='circle'){
-            if(rest[0]==rest[1]){
-                return Math.PI;
+            if(rest[rest.length-1]===1 || rest.length===1){
+                return Math.pow(rest[rest.length-1],2)*Math.PI;
             }
             return Math.PI*Math.pow(rest[1],2) + getArea(shape, rest[0], rest[1]-1);
         }
@@ -12,7 +12,10 @@ function getArea(shape, ...rest) {
         return (rest[0]+rest[1])*(rest[2]/2);
         }
 }
+
 console.log(getArea('circle', 1 ,1));
+console.log(getArea('circle', 2));
+console.log(getArea('circle', 3));
 console.log(getArea('circle', 1 ,2));
 console.log(getArea('circle', 1 ,3));
 console.log(getArea('circle', 1 ,4));
