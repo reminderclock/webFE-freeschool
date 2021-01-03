4c3c27c20fd8d3e309b3d6a3f9ff2dde62c8b5c7
reminderclock <reminderclock@naver.com>
2021-01-04 01:30:03 +0900

    Feat: [getArea] 계산 수행 순서 출력하기
    printExecutionSequence(): 호출시 getArea에 쌓아둔 data 출력

--- getArea/main.js
+++ getArea/main.js
@@ -1,19 +1,31 @@
+const array=[];
 function getArea(shape, ...rest) {
-        if(shape==='circle'){
-            if(rest[rest.length-1]===1 || rest.length===1){
-                return Math.pow(rest[rest.length-1],2)*Math.PI;
-            }
-            return Math.PI*Math.pow(rest[1],2) + getArea(shape, rest[0], rest[1]-1);
+    if(shape==='circle'){
+        if(rest[rest.length-1]===1 || rest.length===1){
+            array.push(shape);
+            return Math.pow(rest[rest.length-1],2)*Math.PI;
         }
-        else if(shape === 'rect'){
-        return rest[0]*rest[1];
-        }
-        else if(shape === 'trapezoid'){
-        return (rest[0]+rest[1])*(rest[2]/2);
-        }
+        return Math.PI*Math.pow(rest[1],2) + getArea(shape, rest[0], rest[1]-1);
+    }
+    else if(shape === 'rect'){
+    array.push(shape);
+    return rest[0]*rest[1];
+    }
+    else if(shape === 'trapezoid'){
+    array.push(shape);
+    return (rest[0]+rest[1])*(rest[2]/2);
+    }
+    else if(shape === 'call') {
+        console.log(array.join(', '));
+    }
+}
+
+function printExecutionSequence() {
+    getArea('call');
 }
 
 console.log(getArea('circle', 1 ,1));
+console.log(getArea('rect', 10,15));
 console.log(getArea('circle', 2));
 console.log(getArea('circle', 3));
 console.log(getArea('circle', 1 ,2));
@@ -21,3 +33,4 @@ console.log(getArea('circle', 1 ,3));
 console.log(getArea('circle', 1 ,4));
 console.log(getArea('rect', 10,15));
 console.log(getArea('trapezoid', 10,15,12));
+printExecutionSequence();
